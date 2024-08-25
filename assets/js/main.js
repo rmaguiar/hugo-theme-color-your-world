e7f49a8c69f4b1c3f3e87a52736fdeb8b188abc7
Nuno Nebeker <nebeker@users.noreply.github.com>
2024-08-25 16:10:31 +0100

    Revert "Fix capitalization." Fix indentation erros from editor. This reverts commit a72296c4c3e3d9957aeeb8d3cfd620afcd516a05.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
 // Get default accent colors
-{ { $defaultDarkAccent:= .Site.Params.Style.darkAccent | default .Site.Data.default.style.darkAccent } }
-{ { $defaultLightAccent:= .Site.Params.Style.lightAccent | default .Site.Data.default.style.lightAccent } }
+{{ $defaultDarkAccent   := .Site.Params.Style.darkAccent  | default .Site.Data.default.style.darkAccent }}
+{{ $defaultLightAccent  := .Site.Params.Style.lightAccent | default .Site.Data.default.style.lightAccent }}
 
 // Get CSS transition
-{ { $changeTransition:= .Site.Params.Style.changeTransition | default .Site.Data.default.style.changeTransition } }
+{{ $changeTransition := .Site.Params.Style.changeTransition | default .Site.Data.default.style.changeTransition }}
 
 // =================================================
 // Mode switcher + Custom accent color
@@ -27,7 +27,7 @@ function setLight() {
 /*
  * Initialization triggers dark/light mode based on 3 things
  * The priority follows:
- *
+ * 
  * 1. Local preference (localStorage)
  * 2. System settings (prefers-color-scheme)
  * 3. HTML data-* attribute (data-mode)
@@ -35,65 +35,65 @@ function setLight() {
 
 const localMode = localStorage.getItem('mode');
 
-{ { if not.Site.Params.Style.ignoreSystemSettings } }
-const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
-const prefersLight = window.matchMedia('(prefers-color-scheme: light)');
-{ { end } }
+{{ if not .Site.Params.Style.ignoreSystemSettings }}
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+  const prefersLight = window.matchMedia('(prefers-color-scheme: light)');
+{{ end }}
 
 if (localMode === 'dark') {
   setDark();
 } else if (localMode === 'light') {
   setLight();
 
-  { { if not.Site.Params.Style.ignoreSystemSettings } }
-} else if (prefersDark.matches) {
-  setDark();
-} else if (prefersLight.matches) {
-  setLight();
-  { { end } }
+  {{ if not .Site.Params.Style.ignoreSystemSettings }}
+    } else if (prefersDark.matches) {
+      setDark();
+    } else if (prefersLight.matches) {
+      setLight();
+  {{ end }}
 
 }
 
 
-{ { if .hugo.IsServer } }
+{{ if .Hugo.IsServer }}
 
-function capitalize(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
+  function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 
-const currentModeTitle = capitalize(rootElement.getAttribute('data-mode'));
+  const currentModeTitle = capitalize(rootElement.getAttribute('data-mode'));
 
-if (localMode !== null) {
-  console.log(currentModeTitle +
+  if (localMode !== null) {
+    console.log(currentModeTitle +
     ' mode loaded via local preference (localStorage).');
-} else if (typeof prefersDark !== 'undefined') {
-  console.log(currentModeTitle +
+  } else if (typeof prefersDark !== 'undefined') {
+    console.log(currentModeTitle +
     ' mode loaded via system settings (prefers-color-scheme).');
-} else {
-  console.log(currentModeTitle +
+  } else {
+    console.log(currentModeTitle +
     ' mode loaded via HTML data-* attribute (data-mode).');
-}
-
-{ { end } }
+  }
+  
+{{ end }}
 
 
 function getAccent() {
-
+  
   const currentMode = rootElement.getAttribute('data-mode');
-
+  
   const localDarkAccent = localStorage.getItem('darkAccent');
   const localLightAccent = localStorage.getItem('lightAccent');
-
+  
   let currentAccent;
 
   if (currentMode === 'dark') {
-
+  
     if (localDarkAccent === null) {
       currentAccent = '{{ $defaultDarkAccent }}';
     } else {
       currentAccent = localDarkAccent;
     }
-
+    
   } else {
 
     if (localLightAccent === null) {
@@ -101,26 +101,26 @@ function getAccent() {
     } else {
       currentAccent = localLightAccent;
     }
-
+    
   }
-
-  { { if .hugo.IsServer } }
-
-  if (
-    (currentMode === 'dark') &&
-    (localStorage.getItem('darkAccent') !== null) ||
-    (currentMode === 'light') &&
-    (localStorage.getItem('lightAccent') !== null)
-  ) {
-    console.log('Custom accent color defined. Loading custom ' +
+  
+  {{ if .Hugo.IsServer }}
+  
+    if (
+      (currentMode === 'dark') &&
+      (localStorage.getItem('darkAccent') !== null) ||
+      (currentMode === 'light') &&
+      (localStorage.getItem('lightAccent') !== null)
+    ) {
+      console.log('Custom accent color defined. Loading custom ' +
       currentMode + ' accent (' + currentAccent + ').');
-  } else {
-    console.log('Custom accent color NOT defined. Loading default ' +
+    } else {
+      console.log('Custom accent color NOT defined. Loading default ' +
       currentMode + ' accent (' + currentAccent + ').');
-  }
-
-  { { end } }
-
+    }
+    
+  {{ end }}
+  
   return currentAccent;
 }
 
@@ -140,10 +140,10 @@ metaThemeColor.setAttribute('content', activeAccent);
 
 
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
 
   const colorPicker = document.querySelector('footer input');
-
+  
   function updateAccent() {
     const activeAccent = getAccent();
 
@@ -152,18 +152,18 @@ document.addEventListener('DOMContentLoaded', function () {
     metaThemeColor.setAttribute('content', activeAccent);
   }
 
-  colorPicker.onchange = function () {
+  colorPicker.onchange = function() {
 
     const selectedAccent = colorPicker.value;
 
     rootStyle.setProperty('--accent', selectedAccent);
-
+    
     if (rootElement.getAttribute('data-mode') === 'dark') {
       localStorage.setItem('darkAccent', selectedAccent);
     } else {
       localStorage.setItem('lightAccent', selectedAccent);
     }
-
+    
     updateAccent();
   }
 
@@ -173,14 +173,14 @@ document.addEventListener('DOMContentLoaded', function () {
   // Smooth transition, only when changing modes (and not loading pages)
   function smoothTransition() {
     document.body.style.transition =
-      document.querySelector('header').style.transition =
-      document.querySelector('footer').style.transition =
-      '{{ printf "background-color %[1]s, color %[1]s" $changeTransition }}';
+    document.querySelector('header').style.transition =
+    document.querySelector('footer').style.transition =
+    '{{ printf "background-color %[1]s, color %[1]s" $changeTransition }}';
   }
-
+  
   // Change mode via localStorage
   function localModeChange() {
-
+  
     smoothTransition();
 
     if (rootElement.getAttribute('data-mode') === 'light') {
@@ -190,63 +190,63 @@ document.addEventListener('DOMContentLoaded', function () {
       setLight();
       localStorage.setItem('mode', 'light');
     }
-
-    { { if .hugo.IsServer } }
-    console.log('Local: ' +
+    
+    {{ if .Hugo.IsServer }}
+      console.log('Local: ' +
       capitalize(localStorage.getItem('mode') + ' mode set.'));
-    { { end } }
-
+    {{ end }}
+    
     updateAccent();
   }
 
+  
+  {{ if .Hugo.IsServer }}
+  
+    // TEST
+    // Keyboard shortcut for mode change, here for testing purposes only
+    // CTRL + ALT + M
+    document.addEventListener('keydown', (event) => {
+      const e = event || window.event;
+      if (e.keyCode === 77 && e.ctrlKey && e.altKey) {
+        localModeChange();
+        return;
+      }
+    }, false);
+    
+  {{ end }}
 
-  { { if .hugo.IsServer } }
 
-  // TEST
-  // Keyboard shortcut for mode change, here for testing purposes only
-  // CTRL + ALT + M
-  document.addEventListener('keydown', (event) => {
-    const e = event || window.event;
-    if (e.keyCode === 77 && e.ctrlKey && e.altKey) {
-      localModeChange();
-      return;
+  {{ if not .Site.Params.Style.ignoreSystemSettings }}
+  
+    // Change mode via system settings
+    function systemModeChange() {
+    
+      smoothTransition();
+      
+      if (prefersDark.matches) {
+        setDark();
+      } else {
+        setLight();
+      }
+      
+      {{ if .Hugo.IsServer }}
+        console.log('System: ' +
+        capitalize(rootElement.getAttribute('data-mode')) + ' mode set.');
+      {{ end }}
+      
+      updateAccent();
+      
+      // System settings do not require localStorage
+      if (localMode !== null) {
+        localStorage.removeItem('mode');
+      }
+
     }
-  }, false);
 
-  { { end } }
-
-
-  { { if not.Site.Params.Style.ignoreSystemSettings } }
-
-  // Change mode via system settings
-  function systemModeChange() {
-
-    smoothTransition();
-
-    if (prefersDark.matches) {
-      setDark();
-    } else {
-      setLight();
-    }
-
-    { { if .hugo.IsServer } }
-    console.log('System: ' +
-      capitalize(rootElement.getAttribute('data-mode')) + ' mode set.');
-    { { end } }
-
-    updateAccent();
-
-    // System settings do not require localStorage
-    if (localMode !== null) {
-      localStorage.removeItem('mode');
-    }
-
-  }
-
-  // System settings listener
-  prefersDark.addEventListener('change', systemModeChange);
-
-  { { end } }
+    // System settings listener
+    prefersDark.addEventListener('change', systemModeChange);
+  
+  {{ end }}
 
   // Mode change button listener
   document.querySelector('footer button')
